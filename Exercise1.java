19595fc1af9c02637c58eb577c2a437d16573f9a
VedantKaushik <158493405+VedantKaushik@users.noreply.github.com>
2024-06-29 00:14:40 +0530

    updating exercise1

--- Exercise1.java
+++ Exercise1.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
 public class Exercise1 {
+    public static void main(String[] args) {
+        Scanner f= new Scanner(System.in);
+
+        System.out.print("Enter the full marks of paper = ");
+        int tm=f.nextInt();
+        int sum =0,i;
+        for ( i = 1; i < 6; i++) {
+            System.out.print("Marks of subject " + i);
+            System.out.print(" = ");
+            int a = f.nextInt();
+            sum=a+sum;
+        }
+        System.out.print("final percentage = "+ (sum*100/(5*tm)));
+        System.out.print("%");
+    }
 }
