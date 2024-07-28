699cf2363e132208ed277f47b1bcc320d29df224
VedantKaushik <vedantkaushik205@gmail.com>
2024-07-28 09:53:45 +0530

    -

---
+++ fewPractise.java
@@ -0,0 +1,47 @@
+//import java.util.Scanner;
+//
+//public class fewPractise {
+//    public static void main(String[] args) {
+//        // Declare variables to store decimal number, quotient, and an array for binary
+//        // digits
+//        int dec_num, quot, i = 1, j;
+//        int bin_num[] = new int[100];
+//
+//        // Create a Scanner object to read input from the user
+//        Scanner scan = new Scanner(System.in);
+//
+//        // Prompt the user to input a decimal number
+////        System.out.print("Input a Decimal Number: ");
+////        dec_num = scan.nextInt();
+////
+////        // Initialize the quotient with the decimal number
+//        quot = 47;
+//
+//
+//
+//        // Display the binary representation of the decimal number
+//        System.out.print("Binary number is: ");
+//        for (j = i - 1; j > 0; j--) {
+//            System.out.print(bin_num[j]);
+//        }
+//        //System.out.print("\n");
+//    }
+//}
+//public class fewPractise {
+//    public static void main(String[] args) {
+//        String str1 = "NPTEL";
+//        String str2 = "java";
+//        int a = 20;
+//        int b = 24;
+//        System.out.println(str1 + a + b); // Statement 1
+//
+//        System.out.println(a + b + str2); // Statement 2
+//    }
+//}
+public class fewPractise {
+    public static void main(String[] args) {
+        int x = 5;
+        x *= (2 + 8);
+        System.out.println(x);
+    }
+}
